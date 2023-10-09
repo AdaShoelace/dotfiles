9b780831a46d6cb03f939889d09b5c3359dd1ffc
AdaShoelace <adashoelace@gmail.com>
2023-10-09 21:54:30 +0200

    Migrate neovim config to lua

---
+++ nvim/lua/plugins/plugins.lua
@@ -0,0 +1,36 @@
+return {
+	"vim-airline/vim-airline",
+	"vim-airline/vim-airline-themes",
+	"scrooloose/nerdtree",
+	"fatih/vim-go",
+	"ctrlpvim/ctrlp.vim",
+	"tpope/vim-surround",
+	"jiangmiao/auto-pairs",
+	"ibhagwan/fzf-lua",
+	"numToStr/FTerm.nvim",
+	"morhetz/gruvbox",
+	"svermeulen/vimpeccable",
+	-- Rust
+	"simrat39/rust-tools.nvim",
+	-- Mason
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+
+	"neovim/nvim-lspconfig",
+
+	-- Completion framework:
+	"hrsh7th/nvim-cmp",
+
+	-- LSP completion source:
+	"hrsh7th/cmp-nvim-lsp",
+
+	-- Useful completion sources:
+	"hrsh7th/cmp-nvim-lua",
+	"hrsh7th/cmp-nvim-lsp-signature-help",
+	"hrsh7th/cmp-vsnip",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/vim-vsnip",
+
+	"nvim-treesitter/nvim-treesitter",
+}
